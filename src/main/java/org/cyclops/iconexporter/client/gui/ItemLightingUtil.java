39bc38a42b1f90fcfc2926546ecf6dc859f6abae
Ruben Taelman <rubensworks@gmail.com>
2019-07-11 14:21:12 +0200

    Add ItemLightingUtil

---
+++ src/main/java/org/cyclops/iconexporter/client/gui/ItemLightingUtil.java
@@ -0,0 +1,48 @@
+package org.cyclops.iconexporter.client.gui;
+
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.util.math.Vec3d;
+
+/**
+ * The same as {@link RenderHelper#enableGUIStandardItemLighting},
+ * but with a scale that can be applied.
+ *
+ * Inspired by mezz's ZoomRenderHelper
+ * https://github.com/mezz/ItemZoom/blob/1.10/src/main/java/mezz/itemzoom/client/ZoomRenderHelper.java
+ * @author rubensworks
+ */
+public class ItemLightingUtil {
+
+    private static final Vec3d LIGHT0_POS = (new Vec3d(0.20000000298023224D, 1.0D, -0.699999988079071D)).normalize();
+    private static final Vec3d LIGHT1_POS = (new Vec3d(-0.20000000298023224D, 1.0D, 0.699999988079071D)).normalize();
+
+    public static void enableGUIStandardItemLighting(float scale) {
+        GlStateManager.pushMatrix();
+        GlStateManager.rotate(-30.0F, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate(165.0F, 1.0F, 0.0F, 0.0F);
+        enableStandardItemLighting(scale);
+        GlStateManager.popMatrix();
+    }
+
+    public static void enableStandardItemLighting(float scale) {
+        GlStateManager.enableLighting();
+        GlStateManager.enableLight(0);
+        GlStateManager.enableLight(1);
+        GlStateManager.enableColorMaterial();
+        GlStateManager.colorMaterial(1032, 5634);
+        GlStateManager.glLight(16384, 4611, RenderHelper.setColorBuffer((float) LIGHT0_POS.x, (float) LIGHT0_POS.y, (float) LIGHT0_POS.z, 0.0f));
+        float lightStrength = 0.3F * scale;
+        GlStateManager.glLight(16384, 4609, RenderHelper.setColorBuffer(lightStrength, lightStrength, lightStrength, 1.0F));
+        GlStateManager.glLight(16384, 4608, RenderHelper.setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GlStateManager.glLight(16384, 4610, RenderHelper.setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GlStateManager.glLight(16385, 4611, RenderHelper.setColorBuffer((float) LIGHT1_POS.x, (float) LIGHT1_POS.y, (float) LIGHT1_POS.z, 0.0f));
+        GlStateManager.glLight(16385, 4609, RenderHelper.setColorBuffer(lightStrength, lightStrength, lightStrength, 1.0F));
+        GlStateManager.glLight(16385, 4608, RenderHelper.setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GlStateManager.glLight(16385, 4610, RenderHelper.setColorBuffer(0.0F, 0.0F, 0.0F, 1.0F));
+        GlStateManager.shadeModel(7424);
+        float ambientLightStrength = 0.4F;
+        GlStateManager.glLightModel(2899, RenderHelper.setColorBuffer(ambientLightStrength, ambientLightStrength, ambientLightStrength, 1.0F));
+    }
+
+}
